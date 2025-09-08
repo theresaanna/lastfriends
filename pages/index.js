f72c33077b9117041f541bb95809d513d49b2cf1
theresa <theresasumma@gmail.com>
2025-09-08 12:26:39 -0700

    Add development mode notice for Spotify API restrictions

--- pages/index.js
+++ pages/index.js
@@ -155,6 +155,37 @@ export default function HomePage() {
           </p>
         </div>
 
+        {/* Development Mode Notice */}
+        <div className="max-w-2xl mx-auto mb-6">
+          <div className="bg-yellow-50 border border-yellow-300 rounded-lg p-4 fade-in-up">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <svg className="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <div className="ml-3">
+                <h3 className="text-sm font-medium text-yellow-800">Spotify Development Mode</h3>
+                <div className="mt-2 text-sm text-yellow-700">
+                  <p>
+                    This app is in development mode for Spotify. If you would like access for Spotify testing, please email{' '}
+                    <a href="mailto:theresasumma@gmail.com" className="font-medium underline hover:text-yellow-600">
+                      theresasumma@gmail.com
+                    </a>
+                    . Priority access given to potential employers to evaluate my work.
+                  </p>
+                  <p className="mt-2">
+                    This restriction is due to an opaque Spotify API policy that prohibits individuals from gaining production access to their API. My apologies for this inconvenience.
+                  </p>
+                  <p className="mt-2 font-medium">
+                    Last.fm integration is still working and available for your perusal.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/* In-app browser notice */}
         <div className="max-w-2xl mx-auto mb-6">
           <InAppBrowserNotice />
